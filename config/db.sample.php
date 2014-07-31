76822cdbc6069f78c1f600236b4d1b2eff658ac9
Matt Clemmer <mclemmer@gmail.com>
2014-07-31 08:59:48 -0400

    Created Sample Config Files

---
+++ config/db.sample.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=mydatabase',
+    'username' => 'user',
+    'password' => 'password',
+    'charset' => 'utf8',
+];
+
