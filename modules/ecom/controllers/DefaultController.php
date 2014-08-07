3dd8546c71f241fdbdc05abca34a707d3bae61b7
Matt Clemmer <mclemmer@gmail.com>
2014-08-07 15:32:26 -0400

    Added Access Control

--- modules/ecom/controllers/DefaultController.php
+++ modules/ecom/controllers/DefaultController.php
@@ -3,11 +3,16 @@
 namespace app\modules\ecom\controllers;
 
 use yii\web\Controller;
+use yii\web\ForbiddenHttpException;
 
 class DefaultController extends Controller
 {
     public function actionIndex()
     {
+		if (!\Yii::$app->user->can('ecomDefaultIndex')) {
+			throw new ForbiddenHttpException('You do not have privileges to view this content.');
+		}
+		
         return $this->render('index');
     }
 }
