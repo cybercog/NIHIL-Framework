549a18c4001e9ac84f4980f40fb2ab40af966f2e
Matt Clemmer <mclemmer@gmail.com>
2014-08-15 13:09:05 -0400

    Removed Ads and Added Site Meta Usage

--- modules/cms/views/pages/view.php
+++ modules/cms/views/pages/view.php
@@ -6,14 +6,14 @@ use yii\widgets\Breadcrumbs;
 /* @var $this yii\web\View */
 /* @var $model app\modules\cms\models\Page */
 
-$this->title = 'uclemmer | ' . $page->name;
+$this->title = Yii::$app->params['siteMeta']['title'] . ' | ' . $page->name;
 $this->registerMetaTag(['description' => $page->description]);
 $this->registerMetaTag(['keywords' => 'uclemmer.com, uriah, clemmer, nihil, nihil corporation']); // Update keywords from tags
 
-$this->registerMetaTag(['og:title' => 'uclemmer.com | ' . $page->name]);
+$this->registerMetaTag(['og:title' => Yii::$app->params['siteMeta']['title'] . ' | ' . $page->name]);
 $this->registerMetaTag(['og:image' => 'http://www.uclemmer.com/favicon/600x600.png']);
 $this->registerMetaTag(['og:url' => 'http://www.uclemmer.com/tutorials/' . $page->slug]);
-$this->registerMetaTag(['og:site_name' => 'uclemmer.com']);
+$this->registerMetaTag(['og:site_name' => Yii::$app->params['siteMeta']['site_name']]);
 $this->registerMetaTag(['og:type' => 'article']);
 $this->registerMetaTag(['og:description' => $page->description]);
 
@@ -43,28 +43,6 @@ $this->params['breadcrumbs'][] = $page->name;
 			<div class="container">
 			
 				<div class="row">
-					<div class="col-xs-12">
-					
-						<div class="visible-xs-block text-center" style="padding-top:15px;">
-						<?php if(!YII_DEBUG) { ?>
-							<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-							<!-- Test Mobile Banner -->
-							<ins class="adsbygoogle"
-								 style="display:inline-block;width:320px;height:50px"
-								 data-ad-client="ca-pub-5089214589271094"
-								 data-ad-slot="9365591705"></ins>
-							<script>
-							(adsbygoogle = window.adsbygoogle || []).push({});
-							</script>
-						<?php }else{ ?>
-							<img src="http://placehold.it/320x50&text=Ad" alt="Ad" />
-						<?php } ?>
-						</div>
-					
-					</div>
-				</div>
-				
-				<div class="row">
 					<div class="col-sm-9">
 					
 						<div class="row">
@@ -88,37 +66,7 @@ $this->params['breadcrumbs'][] = $page->name;
 					</div>
 					<div class="col-sm-3" style="padding-top:15px;padding-bottom:10px;">
 					
-						<div class="hidden-xs">
-						<?php if(!YII_DEBUG) { ?>
-							<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-							<!-- uclemmer Tall -->
-							<ins class="adsbygoogle"
-								 style="display:inline-block;width:160px;height:600px"
-								 data-ad-client="ca-pub-5089214589271094"
-								 data-ad-slot="9079937707"></ins>
-							<script>
-							(adsbygoogle = window.adsbygoogle || []).push({});
-							</script>
-						<?php }else{ ?>
-							<img src="http://placehold.it/160x600&text=Ad" alt="Ad" />
-						<?php } ?>
-						</div>
-						
-						<div class="visible-xs-block text-center">
-						<?php if(!YII_DEBUG) { ?>
-							<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-							<!-- Test Mobile Large Block -->
-							<ins class="adsbygoogle"
-								 style="display:inline-block;width:336px;height:280px"
-								 data-ad-client="ca-pub-5089214589271094"
-								 data-ad-slot="6412125308"></ins>
-							<script>
-							(adsbygoogle = window.adsbygoogle || []).push({});
-							</script>
-						<?php }else{ ?>
-							<img src="http://placehold.it/336x280&text=Ad" alt="Ad" />
-						<?php } ?>
-						</div>
+						&nbsp;
 					
 					</div>
 				</div>
