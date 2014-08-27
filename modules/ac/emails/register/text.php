8e6053275c4bf8b9b19b27ad2c2f37c44558a7f4
Uriah M. Clemmer IV <mclemmer@gmail.com>
2014-08-27 10:00:39 -0400

    Updated Email Directory Structure

---
+++ modules/ac/emails/register/text.php
@@ -0,0 +1,27 @@
+The NIHIL Corporation
+
+Dear <?php echo $user->nickname; ?>,
+Thank you for registering with The NIHIL Corporation.  Use the following code to verify your account:
+
+    Verification Code: <?php echo $authkey->key; ?>
+    
+of go to the following link:
+
+https://www.nihil.co/ac/users/verify/<?php echo $authkey->key; ?>
+
+We look forward to seeing you around.  If you need any more information, or if you have any questions/concerns, you can contact me by replying to this email.
+
+Sincerely,
+Matt Clemmer
+Chief Executive Officer
+                                                                    
+If this email was generated in error, please contact support@nihil.co.
+
+The  NIHIL Corporation
+6409 Sail Pointe Lane
+Hixson, TN 37343
+
+*You received this email because you registered <br />an account with The NIHIL Corporation.
+
+https://www.nihil.co
+https://www.nihil.co/privacy
