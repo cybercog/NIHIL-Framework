916a0885512f0e5b17d46dd0c531fa599ff4b0bb
Uriah M. Clemmer IV <mclemmer@gmail.com>
2014-09-08 19:44:25 -0400

    Added Access Control

--- modules/core/controllers/DefaultController.php
+++ modules/core/controllers/DefaultController.php
@@ -8,6 +8,10 @@ class DefaultController extends Controller
 {
     public function actionIndex()
     {
+		if (!\Yii::$app->user->can('coreDefaultIndex')) {
+			throw new ForbiddenHttpException('You do not have privileges to view this content.');
+		}
+		
         return $this->render('index');
     }
 }
