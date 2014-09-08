17775b3d670ddbe7a4c8e66f2df1533f0bb757b6
Uriah M. Clemmer IV <mclemmer@gmail.com>
2014-09-08 19:42:55 -0400

    Removed Unused Actions

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -50,32 +50,7 @@ class SiteController extends Controller
 
     public function actionIndex()
     {
-        return $this->render('index', [
-			//'projects' => Project::findRecentProjects(3),
-		]);
-    }
-
-    public function actionLogin()
-    {
-        if (!\Yii::$app->user->isGuest) {
-            return $this->goHome();
-        }
-
-        $model = new LoginForm();
-        if ($model->load(Yii::$app->request->post()) && $model->login()) {
-            return $this->goBack();
-        } else {
-            return $this->render('login', [
-                'model' => $model,
-            ]);
-        }
-    }
-
-    public function actionLogout()
-    {
-        Yii::$app->user->logout();
-
-        return $this->goHome();
+        return $this->render('index');
     }
 
     public function actionContact()
