1805731b1cda11ca6ec05927125187a243e105b4
Uriah M. Clemmer IV <mclemmer@gmail.com>
2014-09-08 20:01:28 -0400

    Created Theme Assets

---
+++ assets/ShirlockAsset.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace app\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
+ */
+class ShirlockAsset extends AssetBundle
+{
+    public $basePath = '@webroot';
+    public $baseUrl = '@web';
+    public $css = [
+		//'css/bootstrap/bootstrap.min.css',
+		'css/font-awesome/font-awesome.min.css',
+        'css/shirlock.css',
+    ];
+    public $js = [
+		//'js/jquery/jquery-1.11.1.min.js',
+		//'js/bootstrap/bootstrap.min.js',
+    ];
+    public $depends = [
+        'yii\web\YiiAsset',
+        'yii\bootstrap\BootstrapAsset',
+    ];
+}
