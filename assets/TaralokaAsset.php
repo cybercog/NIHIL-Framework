c9e905b4a265db8b4a266310c680c279eef45a50
Uriah M. Clemmer IV <mclemmer@gmail.com>
2014-09-13 18:03:02 -0400

    Changed Assets

--- assets/TaralokaAsset.php
+++ assets/TaralokaAsset.php
@@ -18,16 +18,16 @@ class TaralokaAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-		//'css/bootstrap/bootstrap.min.css',
+		'css/bootstrap/bootstrap.min.css',
 		'css/font-awesome/font-awesome.min.css',
         'css/taraloka.css',
     ];
     public $js = [
-		//'js/jquery/jquery-1.11.1.min.js',
+		'js/jquery/jquery-1.11.1.min.js',
 		'js/bootstrap/bootstrap.min.js',
     ];
     public $depends = [
-        'yii\web\YiiAsset',
-        'yii\bootstrap\BootstrapAsset',
+        //'yii\web\YiiAsset',
+        //'yii\bootstrap\BootstrapAsset',
     ];
 }
